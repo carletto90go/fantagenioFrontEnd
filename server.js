30d302ad26beee766414de9bec47ebaa399991bf
vinxavi <vinxavi2@gmail.com>
2019-10-12 01:12:32 +0200

    Dai Heroku, forza

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ const path = require('path');
 const cors = require('cors');
 const app = express();
 //app.use(express.static('public'));
-app.use('/', serveStatic(path.join(__dirname, 'dist')));
-app.use(cors());
+
 const port = process.env.PORT || 80;
 app.listen(port, () => {
 	console.log(`Starting server at ${port}`);
 });
+app.use(cors());
+app.use('/', serveStatic(path.join(__dirname, 'dist')));
+
+app.get('/', (req,res) => {
+    response.send("Bella Heroku");
+})
