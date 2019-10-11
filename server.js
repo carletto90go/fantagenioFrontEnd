3d959def7f799e51bb2668e1d78d77007021f564
vinxavi <vinxavi2@gmail.com>
2019-10-11 20:56:55 +0200

    Heroku c'mon

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const serveStatic = require("serve-static")
 const path = require('path');
 const cors = require('cors');
 const app = express();
-//app.use(express.static('public'));
-app.use(serveStatic(path.join("/", 'dist')));
+app.use(express.static('public'));
+//app.use(serveStatic(path.join("/", 'public')));
 app.use(cors());
 const port = process.env.PORT || 80;
 app.listen(port, () => {
