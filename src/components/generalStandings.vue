d6f948209bcb03b695c22bc9a5c4bdfbd9784860
Carlomatteo <carletto90cd@hotmail.it>
2019-10-13 20:58:38 +0200

    aggiunta tabella classifica

--- src/components/generalStandings.vue
+++ src/components/generalStandings.vue
@@ -1,12 +1,33 @@
 <template>
     <div id="generalStandings">
-        <p>QUI CLASSIFICA GENERALE</P>
+        <b-table>
+        </b-table>
     </div>    
 </template>
 <script>
-
+export default{
+    name: 'generalStandings',
+    data(){
+        return{
+            tableDataStandings:{
+                items: tableItems,
+                fields: tableFields
+            }
+        }
+    },
+    mounted(){
+         const options = {
+            method: 'GET',
+            headers: {
+            'Content-Type': 'application/json'
+        }};
+        this.axios.get("https://hidden-ocean-91661.herokuapp.com/api/user/standings", options)
+        .then(
+            for (let i =0; i<10; i++) {
+                    let tableItem = {
+                        "giocatore": response[i].username,
+                        "punti": response[i].points,
+                    }
+        )
+}
 </script>
-
-<style scoped>
-
-</style>
