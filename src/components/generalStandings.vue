00571c14596762be5ec3eb739908c92a1c9dbcfa
vinxavi <vinxavi2@gmail.com>
2019-10-23 12:34:47 +0200

    Fix raddoppio switch pagina

--- src/components/generalStandings.vue
+++ src/components/generalStandings.vue
@@ -1,10 +1,11 @@
 <template>
-    <div id="generalStandings">
-        <b-table id="table" striped hover responsive :items="tableDataStandings.items" :fields="tableDataStandings.fields">
+    <div id="generalStandings" class="text-center">
+        <b-table v-if="!widgetData.tableLoadingSpinner" id="table" striped hover responsive :items="tableDataStandings.items" :fields="tableDataStandings.fields">
          <template v-slot:cell(index)="data">
                 {{ data.index + 1 }}
          </template>   
         </b-table>
+        <b-spinner class="text-center" v-if="widgetData.tableLoadingSpinner" variant="success" label="Spinningg" ></b-spinner>
     </div>    
 </template>
 <script>
@@ -12,6 +13,9 @@ export default{
     name: 'generalStandings',
     data(){
         return{
+            widgetData : {
+                tableLoadingSpinner : false
+            },
             tableDataStandings:{
                 items: tableItems,
                 fields: tableFields
@@ -19,6 +23,7 @@ export default{
         }
     },
     mounted(){
+        this.widgetData.tableLoadingSpinner = true;
          this.$emit("standingsReady", false);
          const jwt = localStorage.getItem("jwt");
          const options = {
@@ -29,16 +34,18 @@ export default{
         }};
         this.axios.get("https://hidden-ocean-91661.herokuapp.com/api/user/standings", options)
         .then( standings =>{
+            standings.data.response.sort( function(a,b){ return b.points - a.points } );
             for(let i =0; i<standings.data.response.length; i++) {
                 let tableItem = {
                     "giocatore": standings.data.response[i].utente.username.toUpperCase(),
                     "punti": standings.data.response[i].points,
-                }
+                    }
                 tableItems.push(tableItem);
                 }
                 this.$emit("standingsReady", true);
+                this.widgetData.tableLoadingSpinner = false;
             })
-        .catch(e=>{
+        .catch( e =>{
             this.$emit("dbError", e);
         });
             
@@ -46,7 +53,7 @@ export default{
      beforeDestroy(){
         this.tableDataStandings.items = null;
         tableItems = [];
-    }
+        }
     }
 let tableItems = [];
 let tableFields = [
@@ -64,7 +71,7 @@ let tableFields = [
       key: 'punti',
       sortable: false,
       label: 'Punti',
-      sortDirection: 'desc'
+      sortDirection: 'desc' //non funziona sta roba
     }
   ];
 </script>
@@ -75,7 +82,6 @@ let tableFields = [
   background:#F1F1F1;
   padding-top:10px;
   padding-left:40px;
-  padding-right:40px; 
-  
+  padding-right:40px;
 }
 </style>
--- src/components/lastGame.vue
+++ src/components/lastGame.vue
@@ -1,14 +1,20 @@
 <template>
-    <div id="lastgame">
-       <b-table  striped hover responsive :items="tableDataLastGame.items" :fields="tableDataLastGame.fields">
-       </b-table> 
+    <div id="lastgame" class="text-center">
+       <b-table v-if="!widgetData.tableLoadingSpinner" striped hover responsive :items="tableDataLastGame.items" :fields="tableDataLastGame.fields">
+       </b-table>
+       <b-spinner class="text-center" v-if="widgetData.tableLoadingSpinner" variant="success" label="Spinningg" ></b-spinner>
     </div>    
 </template>
 <script>
+// https://www.thesportsdb.com/api/v1/json/1/lookupevent.php?id=441613 event by event Id
 export default{
     name: 'lastgame',
     data(){
         return{
+            widgetData : {
+                tableLoadingSpinner : false
+            },
+            round : 1,
             tableDataLastGame:{
                 items: tableItems,
                 fields: tableFields
@@ -17,6 +23,7 @@ export default{
     },
     mounted(){
         this.$emit("lastGameReady", false);
+        this.widgetData.tableLoadingSpinner = true;
         const jwt = localStorage.getItem("jwt");
          const options = {
             method: 'GET',
@@ -24,20 +31,37 @@ export default{
             'Content-Type': 'application/json',
             'auth-token' : jwt
         }};
-        this.axios.get("https://hidden-ocean-91661.herokuapp.com/api/user/myPrediction?round=8", options)
+        this.axios.get("https://hidden-ocean-91661.herokuapp.com/api/user/myPrediction", options)
         .then( resultLastGame =>{
-            // eslint-disable-next-line
-            for(let i =0; i<resultLastGame.data.response.length; i++) {
-                let tableItem = {
-                    "match": resultLastGame.data.response[i].homeTeam + "-" + resultLastGame.data.response[i].awayTeam,
-                    "resultCorrect": resultLastGame.data.response[i].homeGoals + "-" + resultLastGame.data.response[i].awayGoals,
-                    "bet1x2": resultLastGame.data.response[i].bet1x2,
-                    "winResult": resultLastGame.data.response[i].winResult,
-                    "win1x2": resultLastGame.data.response[i].win1x2,
-                }
-                tableItems.push(tableItem);
-                }
-            this.$emit("lastGameReady", true);
+            this.round = resultLastGame.data.response[0].round;
+            const optionsThe = {
+                        method: 'GET',
+                        headers: {
+                        'Content-Type': 'application/json',
+                    }};
+            this.axios.get("https://www.thesportsdb.com/api/v1/json/1/eventsround.php?id=4332&r="+this.round+"&s=1920", optionsThe)
+            .then( theSportsDb => {
+                for(let i = 0; i<theSportsDb.data.events.length; i++) {
+                    let tableItem = {
+                        "match": theSportsDb.data.events[i].strEvent,
+                        "resultCorrect": theSportsDb.data.events[i].intHomeScore + " - " + theSportsDb.data.events[i].intAwayScore
+                        }
+                    resultLastGame.data.response.forEach( match => {
+                        if(match.idMatch == theSportsDb.data.events[i].idEvent){
+                            tableItem.betCR = match.homeGoals.toString() + " - " + match.awayGoals.toString();
+                            tableItem.bet1x2 = match.bet1x2;
+                            console.log(match);
+                            tableItem.points = 0;
+                            if(match.win1x2) tableItem.points += 1;
+                            if(match.winResult) tableItem.points += 3;
+                        }
+                    });
+
+                    tableItems.push(tableItem);
+                    }
+                this.$emit("lastGameReady", true);
+                this.widgetData.tableLoadingSpinner = false;
+                });
             })
             .catch(e => {
                 this.$emit("dbError", e);
@@ -53,31 +77,38 @@ let tableFields = [
      {
       key: 'match',
       sortable: false,
-      label: 'Giocatore'
+      label: 'Partita'
     },
     {
       key: 'resultCorrect',
       sortable: false,
-      label: 'Risultato esatto'
+      label: 'Esito Incontro'
     },
     {
       key: 'bet1x2',
       sortable: false,
-      label: 'Risultato 1X2'
+      label: '1 X 2 Pronosticato'
     },
     {
-      key: 'winResult',
+      key: 'betCR',
       sortable: false,
-      label: 'Risultato esatto'
+      label: 'Risultato Pronosticato'
     },
     {
-      key: 'win1x2',
+      key: 'points',
       sortable: false,
-      label: 'Risultato esatto'
-    },
+      label: 'Punti Guadagnati'
+    }
   ];
 </script>
 
 <style scoped>
-
+#lastgame{
+  width:60%;
+  margin: 30px auto;
+  background:#F1F1F1;
+  padding-top:10px;
+  padding-left:40px;
+  padding-right:40px;
+}
 </style>
