e2b59b0a01c918d0aca9f116e800ad46ae1f63d7
vinxavi <vinxavi2@gmail.com>
2019-10-28 11:56:23 +0100

    Giornata precedente spettacolo

--- src/components/bet.vue
+++ src/components/bet.vue
@@ -190,8 +190,12 @@ export default {
                     this.sendMatchesData.sendDataResponse = response.data;
                     this.sendMatchesData.postMatches = { request : [] };
                     this.widgetData.sendSpinner = false;
+                    this.$emit("betReady", true);
                     this.toggleSubmitModal();
-                });
+                })
+                .catch(e) {
+                    this.$emit("dbError", e);
+                };
         }
     }
 }
